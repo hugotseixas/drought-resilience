9373718aa1f12c63123a0d802088f0ad1d97964d
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-11 08:44:06 -0300

    Major change. Removed not used variables. Values are now calculated over monthly sums, and only the yearly sums are calculated now.

--- analysis/2_merge_data.R
+++ analysis/2_merge_data.R
@@ -1,109 +1,78 @@
-## ------------------------------------------------------------------------- ##
-####* ------------------------------- HEADER ---------------------------- *####
-## ------------------------------------------------------------------------- ##
-##
-## Script name:   Merge data
-##
-## Description:   In this routine, we open NetCDF files extracted from NOAH-MP  
-##                output and extract information of some variables to a tabular 
-##                format, the final product is one table with information
-##                of all scenarios produced by the model.              
-##
-## Author:        Hugo Tameirao Seixas
-## Contact:       hugo.seixas@alumni.usp.br / tameirao.hugo@gmail.com
-##
-## Date created:  Fourth quarter 2019
-## Last update:   2020-06-12
-## Last tested:   2020-06-12
-##
-## Copyright (c) Hugo Tameirao Seixas, 2020
-##
-## ------------------------------------------------------------------------- ##
-##
-## Notes:         Versions of software and libraries used in this  
-##                routine are detailed in the readme file located  
-##                in the project directory.              
-##
-## ------------------------------------------------------------------------- ##
-##
-options(scipen = 6, digits = 4) # View outputs in non-scientific notation
-##
-## ------------------------------------------------------------------------- ##
-##
-## Load libraries:
-##
+# HEADER ----------------------------------------------------------------------
+#
+# Title:        Merge data
+# Description:  In this routine, we open NetCDF files extracted from NOAH-MP  
+#               output and extract information of some variables to a tabular 
+#               format, the final product is one table with information
+##              of all scenarios produced by the model.
+#
+# Author:       Hugo Tameirao Seixas
+# Contact:      tameirao.hugo@gmail.com
+# Date:         2019-05-01
+#
+# Notes:        Versions of software and libraries used in this  
+#               routine are detailed in the documentation located  
+#               in the project directory.      
+#
+# LIBRARIES -------------------------------------------------------------------
+#
 library(tidync)
 library(tibbletime)
 library(lubridate)
+library(fs)
+library(arrow)
 library(tidyverse)
-##
-## ------------------------------------------------------------------------- ##
-####* ------------------------------- CODE ------------------------------ *####
-## ------------------------------------------------------------------------- ##
+#
+# OPTIONS ---------------------------------------------------------------------
+#
 
-#### ---------------------------------------- Open and process NetCDF data #### 
+#
+# OPEN AND PROCESS NETCDF DATA ------------------------------------------------
 
-####' ----- Function to process NetCDF data #### 
-## Function to open NetCDF files, extract and organize information, 
-## and finally merge everything into one tibble
-merge_data <- function(scen) {
-  
-  ## Open NetCDF file corresponding to one scenario
-  nc <- tidync(paste('./scenarios/', scen, sep = '')) 
-  
-  ## Transform data into tibble and calculate yearly values
-  nc <- nc %>%
-    activate('D2,D3,D0') %>% # Activate dimensions of variables of interest
-    hyper_tibble() %>% # Transform the tidync object into a tibble
-    mutate(Times = ymd(Times)) %>%
-    rename(Time = Times, Precip = RAINRATE, Temp = TRAD) %>% 
-    ## Transform daily sums to daily means to some variables 
-    ## (check the pre-processing routine to understand why)
-    mutate_at(vars(Temp, LAI, LH), ~./24) %>%
-    ## Remove one huge outlier that appeared in some scenarios
-    mutate_at(vars(GPP, LH, Temp), ~replace(., abs(.) > 2000, NA)) %>% 
-    as_tbl_time(index = Time) %>% 
-    collapse_by(period = 'yearly') %>%
-    group_by(Time, x, y) %>%
-    summarise_at(
-      vars(Precip, Temp, GPP, LAI, LH), 
-      list(sum = sum, mean = mean), 
-      na.rm = TRUE
-      ) %>% # Calculate sum and mean for the extracted variables
-    select(-c(Precip_mean, Temp_sum, GPP_mean, LAI_sum, LH_sum)) %>%
-    rename(
-      Precip = Precip_sum, 
-      Temp = Temp_mean, 
-      GPP = GPP_sum, 
-      LAI = LAI_mean, 
-      LH = LH_mean
-      ) %>%
-    ungroup() %>% # Ungroup variables
-    mutate(Time = floor_date(Time, unit = 'year')) %>% 
-    as_tibble() %>% 
-    group_by(Time) %>%
-    mutate(Scenario = str_remove(scen, '.nc'))
-  
-  return(nc) # Return the resulting tibble
-  
-}
+scen_list <- dir_ls("data/noah_out/scenarios/")
 
-####' ----- Create a list of all NetCDF files from NOAH-MP ####
-scen_list <- dir('./data/scenarios/') 
+scen_year <-
+  map_df(
+    .x = scen_list,
+    .f = 
+      ~ {
+        
+        # Open NetCDF file corresponding to one scenario
+        nc <- tidync(.x)
+        
+        # Transform data into tibble and calculate yearly values
+        nc_table <- 
+          nc %>%
+          # Activate dimensions of variables of interest
+          activate('D2,D3,D0') %>% 
+          hyper_tibble() %>% # Transform the tidync object into a tibble
+          mutate(Times = ymd(Times)) %>%
+          rename(time = Times, precip = RAINRATE, gpp = GPP) %>% 
+          # Remove one huge outlier that appeared in some scenarios
+          mutate_at(vars(gpp), ~replace(., abs(.) > 2000, NA)) %>% 
+          as_tbl_time(index = time) %>% 
+          collapse_by(period = 'yearly') %>% # Group by year
+          group_by(time, x, y) %>%
+          summarise_at( # Calculate yearly sum
+            vars(precip, gpp), 
+            ~ sum(., na.rm = TRUE)
+          ) %>%
+          ungroup() %>% # Ungroup variables
+          mutate(time = floor_date(time, unit = 'year')) %>% 
+          as_tibble() %>% 
+          mutate( # Create column with scenario name
+            scenario = str_extract(
+              .x, 
+              "(?<=data/noah_out/scenarios/).*(?=.nc)"
+            )
+          )
+        
+      }
+  )
 
-####' ----- Map the function "merge_data" to all files ####
-scen_year <- {
-  as_vector(scen_list) %>%
-    map_df(merge_data)
-}
+# SAVE RESULTS ----------------------------------------------------------------
 
-#### -------------------------------------------------------- Save results ####
-
-####' ----- Save the results as a comma delimited file #### 
-## (this file will be loaded in the next script: calculate_metrics.R)
+# Save the results as a comma delimited file
+# (this file will be loaded in the next script: calculate_metrics.R)
 scen_year %>%
-  write_delim('./data/netcdf_year.txt', delim = ',')
-
-## ------------------------------------------------------------------------- ##
-####* ------------------------------- END ------------------------------- *####
-## ------------------------------------------------------------------------- ##
+  write_parquet('./data/netcdf_year.parquet')
