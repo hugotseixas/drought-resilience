79e7c1296e5414c45d9bf906d628945e0f41dcf0
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-12 19:19:36 -0300

    Add auxiliary data to merged table

--- analysis/2_merge_data.R
+++ analysis/2_merge_data.R
@@ -27,10 +27,80 @@ library(tidyverse)
 #
 
 #
-# OPEN AND PROCESS NETCDF DATA ------------------------------------------------
+# MODEL AUXILIARY OUTPUT -------------------------------------------------
 
+## Open HRLDAS setup file ----
+setup <- tidync('./data/HRLDAS_setup_2015010100_d1')
+
+## Extract information from HRLDAS setup file ----
+
+### Extract vegetation type info from HRLDAS setup file ----
+veg_type <- setup %>%
+  activate(IVGTYP) %>% # Activate vegetation type dimensions
+  hyper_tibble() %>% # Transform into a tibble
+  select(-Time) %>% 
+  ## Rename coordinate variables to match NOAH's output files
+  rename(x = west_east, y = south_north, veg_type = IVGTYP) %>% 
+  arrange(x, y) %>% 
+  mutate(id = row_number())
+
+### Extract soil type info from HRLDAS setup file ----
+soil_type <- setup %>%
+  activate(ISLTYP) %>% # Activate soil type dimensions
+  hyper_tibble() %>% # Transform into a tibble
+  select(-Time) %>% 
+  ## Rename coordinate variables to match NOAH's output files
+  rename(x = west_east, y = south_north, soil_type = ISLTYP) %>% 
+  arrange(x, y) %>% 
+  mutate(id = row_number())
+
+### Extract coordinates info from HRLDAS setup file ----
+coordinates <- setup %>%
+  activate('D2,D3,D0') %>% # Activate coordinates dimensions and variables
+  hyper_tibble() %>% 
+  select(XLAT, XLONG, west_east, south_north) %>% 
+  rename(x = west_east, y = south_north, lat = XLAT, lon = XLONG) %>% 
+  arrange(x, y) %>% 
+  mutate(id = row_number())
+
+## Join aux tables ----
+aux <- 
+  reduce(
+    list(coordinates, soil_type, veg_type),
+    full_join, 
+    by = c("id", "x", "y")
+  )
+
+# NETCDF DATA -----------------------------------------------------------------
+
+## Create scenarios list ----
 scen_list <- dir_ls("data/noah_out/scenarios/")
 
+## Characterize scenarios ----
+scen_table <-
+  tibble(
+    scenario = str_extract(
+      scen_list, 
+      "(?<=data/noah_out/scenarios/).*(?=.nc)"
+    ),
+    drought_intensity = factor(
+      x = c("N", rep(c("D1", "D2", "D3"), each = 16)), 
+      levels = c("N", "D1", "D2", "D3")
+    ),
+    recovery_intensity = factor(
+      c("N", rep(c("N", "W1", "W2", "W3"), each = 4, times = 3)), 
+      levels = c("N", "W1", "W2", "W3")
+    ),
+    drought_duration = c(0, factor(rep(4:1, times = 12)))
+  ) %>%
+  unite(
+    'scen_code', 
+    drought_intensity:drought_duration, 
+    sep = '-', 
+    remove = FALSE
+  )
+
+## Merge nc files into one table ----
 scen_year <-
   map_df(
     .x = scen_list,
@@ -70,9 +140,14 @@ scen_year <-
       }
   )
 
-# SAVE RESULTS ----------------------------------------------------------------
+# MERGE AND SAVE RESULTS ------------------------------------------------------
 
-# Save the results as a comma delimited file
+## Join tables ----
+scen_year <- scen_year %>%
+  left_join(scen_table, by = "scenario") %>%
+  left_join(aux, by = c("x", "y"))
+
+## Save the results as a parquet file ----
 # (this file will be loaded in the next script: calculate_metrics.R)
 scen_year %>%
   write_parquet('./data/netcdf_year.parquet')
